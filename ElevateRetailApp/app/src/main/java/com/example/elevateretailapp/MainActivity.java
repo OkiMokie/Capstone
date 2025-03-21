4e5d343cd5d237ae60b40ec753719452f6a73000
fflores0175 <fflores0175@yahoo.com>
2025-03-21 19:27:15 -0400

    Version 1.2 (edited toolbar)

--- ElevateRetailApp/app/src/main/java/com/example/elevateretailapp/MainActivity.java
+++ ElevateRetailApp/app/src/main/java/com/example/elevateretailapp/MainActivity.java
@@ -17,6 +17,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.appcompat.widget.SearchView;
 
 
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
