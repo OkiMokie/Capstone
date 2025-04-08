1d4cff1f697fc4087050c3209f21ff07a3a5da5d
F1R3BR4ND <eduardorivera305@yahoo.com>
2025-04-07 22:08:23 -0400

    The close button for settings
    Tried to make the closeSettings button work. It's image button and don't see why it won't work... pain

--- app/src/main/java/com/example/elevateretailapp/SettingsFragment.java
+++ app/src/main/java/com/example/elevateretailapp/SettingsFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,5 +62,9 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        //Reference the button
+        ImageButton closeSetting = view.findViewById((R.id.btnCheckout));
+
     }
 }
